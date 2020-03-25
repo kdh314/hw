0219ef48c5a128eb276d2f26e3afdce3d97ff77b
Kang, Donghyun <xnd314@gmail.com>
2020-03-25 14:48:01 +0900

    sum1, sum2 과제

---
+++ sum1.js
@@ -0,0 +1,7 @@
+let i =1;
+let sum =0;
+while (i<=55){
+    sum += i;
+    ++i;
+    }
+    console.log(sum);
---
+++ sum2.js
@@ -0,0 +1,10 @@
+let i = 1;
+let sum = 0;
+while(i <= 55){
+    if ( i % 3 == 0){    
+        sum += i;
+    }
+    if ( i <= 55 )
+        ++i;
+    }
+    console.log(sum);
